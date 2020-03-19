57de2f4af3b3be79278a293731fb9ea8563aa0f6
Cirtap1998 <62359435+Cirtap1998@users.noreply.github.com>
2020-03-19 01:59:43 +0100

    Security Checks
    Added checks for players and location, if something goes wrong I added an error-message!

--- src/me/John_H_Smith/SilentTeleport/Main.java
+++ src/me/John_H_Smith/SilentTeleport/Main.java
@@ -29,18 +29,30 @@ public class Main extends JavaPlugin {
 		
 		if(sender instanceof Player) {
 			Player player = (Player) sender;
-			String noperm = ChatColor.RED+"Not enough permissions";
+			String noperm = ChatColor.RED + "Not enough permissions";
+			String wrongPlayer = " is not a player";
 			
 			if(cmd.getName().equalsIgnoreCase("silentteleport")) {
 				if(player.hasPermission("stp.stp")) {
 					if(args.length == 1) {
 						Player ziel = Bukkit.getPlayer(args[0]);
-						player.teleport(ziel);
+						if(ziel != null) 
+							player.teleport(ziel);
+						else
+							player.sendMessage(ChatColor.RED + args[0] + wrongPlayer);
 						return true;
 					}
 					if(args.length == 2) {
 						Player ziel1 = Bukkit.getPlayer(args[0]);
 						Player ziel2 = Bukkit.getPlayer(args[1]);
+						if(ziel1 == null) {
+							player.sendMessage(ChatColor.RED + args[0] + wrongPlayer);
+							return true;
+						}
+						if(ziel2 == null) {
+							player.sendMessage(ChatColor.RED + args[1] + wrongPlayer);
+							return true;
+						}
 						ziel1.teleport(ziel2);
 						return true;
 					}
@@ -50,10 +62,15 @@ public class Main extends JavaPlugin {
 			if(cmd.getName().equalsIgnoreCase("silentteleportposition")) {
 				if(player.hasPermission("stp.stppos")) {
 					if(args.length == 3) {
-						int CoordsX = Integer.parseInt(args[0]);
-						int CoordsY = Integer.parseInt(args[1]);
-						int CoordsZ = Integer.parseInt(args[2]);
-						Location ziel = new Location(player.getWorld(), CoordsX+0.5, CoordsY+0.5, CoordsZ+0.5);
+						try {
+							int coordsX = Integer.parseInt(args[0]);
+							int coordsY = Integer.parseInt(args[1]);
+							int coordsZ = Integer.parseInt(args[2]);
+						} catch(Exception e) {
+							player.sendMessage(ChatColor.RED + "Wrong format. Check the coordinates");
+							return true;
+						}
+						Location ziel = new Location(player.getWorld(), coordsX+0.5, coordsY+0.5, coordsZ+0.5);
 						player.teleport(ziel);
 						return true;
 					}
@@ -64,7 +81,10 @@ public class Main extends JavaPlugin {
 				if(player.hasPermission("stp.stphere")) {
 					if(args.length == 1) {
 						Player ziel = Bukkit.getPlayer(args[0]);
-						ziel.teleport(player);
+						if(ziel != null) 
+							ziel.teleport(player);
+						else 
+							player.sendMessage(ChatColor.RED + args[0] + wrongPlayer);
 						return true;
 					}
 				} else { player.sendMessage(noperm); return true; }
